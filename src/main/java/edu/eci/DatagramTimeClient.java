1169023d63d337eac6ae81592a7be06b7d992d04
CarlosCL98 <carmer.cl98@gmail.com>
2019-01-29 00:16:27 -0500

    Terminado laboratorio 2.

--- src/main/java/edu/eci/DatagramTimeClient.java
+++ src/main/java/edu/eci/DatagramTimeClient.java
@@ -15,43 +15,33 @@ import java.util.logging.Logger;
 public class DatagramTimeClient {
 
 	public static void main(String[] args) {
-		byte[] sendBuf = new byte[256];
 		String received = "";
-		try {
-			DatagramSocket socket = new DatagramSocket();
-			byte[] buf = new byte[256];
-			InetAddress address = InetAddress.getByName("127.0.0.1");
-			DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 45000);
-			socket.setSoTimeout(2000);
-			while(true){				
+		while (true) {
+			try {
+				DatagramSocket socket = new DatagramSocket();
+				byte[] buf = new byte[256];
+				InetAddress address = InetAddress.getByName("127.0.0.1");
+				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 45000);
 				socket.send(packet);				
-				packet = new DatagramPacket(buf, buf.length);		
-				//System.out.println("1");
-				try {					
+				packet = new DatagramPacket(buf, buf.length);
+				try {
+					socket.setSoTimeout(5000);					
 					socket.receive(packet);
-					//System.out.println("LA address: " + packet.getAddress());
+					received = new String(packet.getData(), 0, packet.getLength());
+					System.out.println("Date: " + received);
 				} catch (SocketTimeoutException ex) {
-					while(packet.getAddress() == null) {
-						//System.out.println("LA address: " + packet.getAddress());	
-						System.out.println("LA MISMA: " + received);
-						Thread.sleep(2000);
-					}
-					socket.send(packet);				
-					packet = new DatagramPacket(buf, buf.length);	
+					System.out.println("Date-SinServer: " + received);
 				}
-				//System.out.println(socket.getSoTimeout());
-				received = new String(packet.getData(), 0, packet.getLength());
-				System.out.println("Date: " + received);
-				Thread.sleep(2000);
+				Thread.sleep(5000);
+			} catch (SocketException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (UnknownHostException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (IOException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
 			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}catch (SocketException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (UnknownHostException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (IOException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-		} 
+		}
 	}
 }
